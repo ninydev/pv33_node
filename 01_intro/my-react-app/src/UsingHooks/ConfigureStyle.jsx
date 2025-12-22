07a78c9fa06e8e062e4ed44da474c6a2a1ee57fc
Oleksandr Nykytin <alex_n@alma-soft.com>
2025-12-22 19:50:31 +0200

    useState

--- 01_intro/my-react-app/src/UsingHooks/ConfigureStyle.jsx
+++ 01_intro/my-react-app/src/UsingHooks/ConfigureStyle.jsx
@@ -1,17 +1,21 @@
 import {useState} from "react";
+import t from './themes.module.css';
 
 export  const ConfigureStyle = () => {
+
+    const [theme, setTheme] = useState('light');
 
     const [bgColor, setBgColor] = useState('#ffffff');
     const [fontSize, setFontSize] = useState(16);
     const [color, setColor] = useState('#000000');
 
     return(
-        <div style={{backgroundColor: bgColor}}>
+        <div className={t[theme]}  style={{backgroundColor: bgColor}}>
             <h1 style={{fontSize: fontSize, color: color }}> Configure Styles</h1>
             <input type="color" value={bgColor} onChange={(e) => setBgColor(e.target.value)}/><br/>
             <input type="number" value={fontSize} onChange={(e) => setFontSize(Number(e.target.value))}/><br/>
             <input type="color" value={color} onChange={(e) => setColor(e.target.value)}/><br/>
+            <button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>Change Theme</button>
         </div>
     )
 }
