27a35f9ce69cc1195c9efff8a65aaefc744eacaf
Oleksandr Nykytin <alex_n@alma-soft.com>
2025-12-22 20:23:14 +0200

    useEffect

--- 01_intro/my-react-app/src/UsingHooks/ConfigureStyle.jsx
+++ 01_intro/my-react-app/src/UsingHooks/ConfigureStyle.jsx
@@ -1,13 +1,38 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import t from './themes.module.css';
 
 export  const ConfigureStyle = () => {
+
+    const [mode, setMode] = useState('load');
 
     const [theme, setTheme] = useState('light');
 
     const [bgColor, setBgColor] = useState('#ffffff');
     const [fontSize, setFontSize] = useState(16);
     const [color, setColor] = useState('#000000');
+
+    useEffect(() => {
+        console.log("Я запускаюсь після кожного рендеру!");
+    });
+
+    useEffect(() => {
+        console.log("Компонент з'явився на екрані (монтування)");
+        // Ідеально для запитів до API
+    }, []);
+
+    useEffect(() => {
+        document.title = `Сторинка перебуває у режімі: ${mode}`;
+    }, [mode]);
+
+    if (mode === 'load') {
+        setTimeout(() => setMode('ready'), 3000);
+    }
+
+    if (mode === 'error') return <div>Error</div>;
+
+    if (mode === 'load') return <div>Loading...</div>;
+
+    // if (mode === 'ready')
 
     return(
         <div className={t[theme]}  style={{backgroundColor: bgColor}}>
