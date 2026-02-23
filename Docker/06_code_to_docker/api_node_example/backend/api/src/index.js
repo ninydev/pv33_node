86a08776ff19f9dbfc3c12d51f405e8e665f6058
Oleksandr Nykytin <alex_n@alma-soft.com>
2026-02-23 19:27:50 +0200

    First Api Server

---
+++ Docker/06_code_to_docker/api_node_example/backend/api/src/index.js
@@ -0,0 +1,26 @@
+const http = require('http');
+
+// Читаем переменные окружения
+const PORT = process.env.PORT || 3000;
+const SERVER_NAME = process.env.SERVER_NAME || 'Default-Server';
+
+const server = http.createServer((req, res) => {
+    // Устанавливаем заголовок, что возвращаем JSON
+    res.setHeader('Content-Type', 'application/json; charset=utf-8');
+
+    // Формируем ответ
+    const data = {
+        status: "OK",
+        server_name: SERVER_NAME,
+        port: PORT,
+        url: req.url, // какой путь запросили
+        method: req.method // GET, POST и т.д.
+    };
+
+    // Отправляем JSON
+    res.end(JSON.stringify(data, null, 2));
+});
+
+server.listen(PORT, () => {
+    console.log(`🚀 Сервер "${SERVER_NAME}" запущен на порту ${PORT}`);
+});
